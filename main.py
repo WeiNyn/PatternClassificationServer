5a52fc423a687523ff5969e30dc23895c10ea131
Tuan <tuan.cao@dounets.com>
2021-05-17 09:12:43 +0700

    add demo and demo-path route for server

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import json
 from typing import Optional
 import os
+from io import BytesIO
 import sys
 from os import path
 import asyncio
 
 sys.setrecursionlimit(10**6)
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -15,8 +17,9 @@ from PIL import Image
 import numpy as np
 
 from src.model import PatternClassifier
+from src.setting import Setting
 
-model = PatternClassifier(sample_dir='image_120_wider_sub/', model_path='model/60_net_.pth')
+model = PatternClassifier(sample_dir=Setting.sample_dir, model_path=Setting.model, device=Setting.device, max_image_size=Setting.max_image_size)
 
 app = FastAPI()
 
@@ -24,6 +27,73 @@ class Data(BaseModel):
     image: str
     image_mask: str
     output_dir: str
+
+@app.post("/demo")
+async def demo_pattern(image: UploadFile = File(...)):
+    try:
+        contents = await image.read()
+        img = Image.open(BytesIO(contents)).convert('RGB')
+    
+    except Exception as ex:
+        return JSONResponse(jsonable_encoder({'error': f"Upload file error {ex}"}), status_code=400)
+    
+    w, h = img.size
+    
+    left = int(3200.0/4032.0*w)
+    right = int(3712.0/4032.0*w)
+    top = int(1700.0/3024.0*h)
+    bottom = int(2212.0/3024.0*h)
+
+    img = img.crop((left, top, right, bottom))    
+    
+    try:
+        result = await model(img, None)
+    
+    except Exception as ex:
+        return JSONResponse(jsonable_encoder({'error': f"Error in detection: {ex}"}), status_code=500)
+    
+    pattern = result['pattern']
+    color = result['color']
+    
+    result = dict(
+        function='pattern_matching',
+        results=dict(
+            pattern_id=pattern['label'],
+            color_code=color['rgb'],
+            color_name=color['color_name'],
+        )
+    )
+    
+    return JSONResponse(jsonable_encoder(result), status_code=200)
+
+
+@app.post("/demo-path")
+async def demo_pattern(image: str): 
+    if not path.exists(image):
+        return JSONResponse(jsonable_encoder({'error': f"{image} is not exists"}), status_code=400)
+    
+    img = Image.open(image).convert('RGB')
+    img = img.crop((3200, 1700, 3712, 2212))
+    
+    try:
+        result = await model(img, None)
+    
+    except Exception as ex:
+        return JSONResponse(jsonable_encoder({'error': f"Error in detection: {ex}"}), status_code=500)
+    
+    pattern = result['pattern']
+    color = result['color']
+    
+    result = dict(
+        function='pattern_matching',
+        results=dict(
+            pattern_id=pattern['label'],
+            color_code=color['rgb'],
+            color_name=color['color_name'],
+        )
+    )
+    
+    return JSONResponse(jsonable_encoder(result), status_code=200)
 
 
 @app.post("/")
@@ -35,7 +105,7 @@ async def pattern(data: Data):
     if not path.exists(image):
         return JSONResponse(jsonable_encoder({'error': f"{image} is not exists"}), status_code=400)
     
-    if not path.exists(image_mask):
+    if len(image_mask) > 0 and not path.exists(image_mask):
         return JSONResponse(jsonable_encoder({'error': f"{image_mask} is not exists"}), status_code=400)
     
     if len(output_dir) > 0 and not path.exists(output_dir):
@@ -43,7 +113,12 @@ async def pattern(data: Data):
     
     
     img = Image.open(image).convert('RGB')
-    mask = Image.open(image_mask)
+    
+    if len(image_mask) > 0:
+        mask = Image.open(image_mask)
+    
+    else:
+        mask = None
     
     result = await model(img, mask)
     
