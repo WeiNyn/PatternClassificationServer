4b134aed81785f37acc2d82310bc716ae2c9eaa3
Tuan <tuan.cao@dounets.com>
2021-06-14 11:14:44 +0700

    add training path

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import json
-from typing import Optional
+from typing import Optional, List
 import os
 from io import BytesIO
 import sys
 from os import path
 import asyncio
+import lpips
+from pydantic.types import Json
+from time import time
+
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, early_stopping
+from pytorch_lightning.loggers import TensorBoardLogger
+from torchvision import transforms
 
 sys.setrecursionlimit(10**6)
 
@@ -18,15 +27,65 @@ import numpy as np
 
 from src.model import PatternClassifier
 from src.setting import Setting
+from src.LPIPSTrainer.src.model.model import PatternModel
+from src.LPIPSTrainer.src.data.data_module import PatternDataModule
 
-model = PatternClassifier(sample_dir=Setting.sample_dir, model_path=Setting.model, device=Setting.device, max_image_size=Setting.max_image_size)
+model = PatternClassifier(sample_dir=Setting.sample_dir, model=Setting.model_path, device=Setting.device, max_image_size=Setting.max_image_size)
 
 app = FastAPI()
+
+
+@app.post("/demo-pipeline")
+async def demo_pipeline(images: List[UploadFile] = File(...), masks: List[UploadFile] = File(...)):
+    star_time = time()
+    if len(images) != len(masks):
+        return JSONResponse(jsonable_encoder({'error': f'Number of masks and images must be the same'}), status_code=400)
+    
+    try:
+        
+        images = [Image.open(BytesIO(await image.read())).convert('RGB') for image in images]
+        masks = [Image.open(BytesIO(await mask.read())) for mask in masks]
+        
+    except Exception as ex:
+        return JSONResponse(jsonable_encoder({'error': f"Upload file error {ex}"}), status_code=400)
+    
+    color_code = None
+    color_name = None
+    pattern_dict = dict()
+    
+    for index, image, mask in zip(range(len(images)), images, masks):
+        result = await model(image, mask)
+        
+        pattern = result['pattern']
+        color = result['color']
+        pattern_id = pattern['label']
+        
+        pattern_dict[pattern_id] = pattern_dict.get(pattern_id, 0) + 1
+        
+        if index == len(images)//2:
+            color_code = color['rgb']
+            color_name = color['color_name']
+    
+    print(pattern_dict)   
+    max_id = max(pattern_dict.keys(), key=lambda x: pattern_dict[x])
+    
+    print(time() - star_time)
+    
+    return dict(
+        function='pattern_matching',
+        results=dict(
+            pattern_id=max_id,
+            color_code=color_code,
+            color_name=color_name
+        )
+    )
+    
 
 class Data(BaseModel):
     image: str
     image_mask: str
     output_dir: str
+
 
 @app.post("/demo")
 async def demo_pattern(image: UploadFile = File(...)):
@@ -66,14 +125,20 @@ async def demo_pattern(image: UploadFile = File(...)):
     
     return JSONResponse(jsonable_encoder(result), status_code=200)
 
+class DataImage(BaseModel):
+    image: str
 
 @app.post("/demo-path")
-async def demo_pattern(image: str): 
+async def demo_pattern(data: DataImage):
+    image = data.image 
     if not path.exists(image):
         return JSONResponse(jsonable_encoder({'error': f"{image} is not exists"}), status_code=400)
     
     img = Image.open(image).convert('RGB')
-    img = img.crop((3200, 1700, 3712, 2212))
+    # img = img.crop((3200, 1700, 3712, 2212))
+    
+    tfs = transforms.RandomCrop(512)
+    img = tfs(img)
     
     try:
         result = await model(img, None)
@@ -160,3 +225,68 @@ async def make_image(pattern, color, output_dir, image_path):
     save_path = path.join(output_dir, image_path.split("/")[-1])
     image.save(save_path)
     
+    
+async def train(model, setting: Setting):
+    model_config = setting.model
+    pattern_config = {k: v for k, v in model_config.items() if k not in ['net', 'version']}
+    
+    model = PatternModel(model=model,
+                         **pattern_config)
+    
+    
+    data_module_config = setting.data_module
+    data_module = PatternDataModule(**data_module_config)
+    
+    checkpoint_config = setting.checkpoint
+    checkpoint_callback = ModelCheckpoint(
+        dirpath='checkpoints',
+        filename='best-checkpoint',
+        verbose=True,
+        mode='min',
+        **checkpoint_config
+    )
+    
+    logger = TensorBoardLogger('lightning_logs', name=setting.logger['name'])
+    
+    early_stopping_config = setting.early_stopping
+    es_enable = early_stopping_config['enable']
+    if es_enable is True:
+        early_stopping_callback = EarlyStopping(**{k: v for k, v in early_stopping_config.items() if k != 'enable'})
+    
+    
+    trainer = pl.Trainer(
+        logger=logger,
+        checkpoint_callback=checkpoint_callback,
+        callbacks=[early_stopping_callback] if es_enable is True else None,
+        progress_bar_refresh_rate=30,
+        **setting.trainer
+    )
+    
+    trainer.fit(model, data_module)
+    
+    return model.model
+
+@app.post('/train')
+async def train_route(path: str):
+    try:
+        global model
+        lpips_model = model.get_model()
+        
+        setting = Setting()
+        setting.data_module['folder'] = path
+        
+        if setting.run_train is True:
+            lpips_model = await train(lpips_model, setting)
+        else:
+            lpips_model = lpips.LPIPS()
+        
+        model = None
+        torch.cuda.empty_cache()
+        
+        setting.sample_dir = path
+        model = PatternClassifier(sample_dir=setting.sample_dir, model=lpips_model, device=setting.device, max_image_size=setting.max_image_size)
+    
+    except Exception as ex:
+        return JSONResponse(jsonable_encoder({'error': f"Error in training model {ex}"}), status_code=500)
+    
+    return JSONResponse(jsonable_encoder({'status': 'successfull'}), status_code=200)
